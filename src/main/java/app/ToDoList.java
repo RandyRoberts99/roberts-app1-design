fa7848a154ceb94e05286090c4403010a950beae
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-10-22 22:10:04 -0400

    Initial GUI Commit

---
+++ src/main/java/app/ToDoList.java
@@ -0,0 +1,10 @@
+package app;
+
+public class ToDoList {
+
+    ToDoListItem[] toDoListObject;
+    public ToDoList(ToDoListItem[] toDoListObject)
+    {
+        this.toDoListObject = toDoListObject;
+    }
+}
---
+++ src/main/java/app/ToDoListItem.java
@@ -0,0 +1,13 @@
+package app;
+
+public class ToDoListItem
+{
+    String description;
+    boolean isCompleted;
+
+    public ToDoListItem(String description, boolean isCompleted)
+    {
+        this.description = description;
+        this.isCompleted = isCompleted;
+    }
+}
